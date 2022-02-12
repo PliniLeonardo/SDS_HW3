413be5b57ed068ae2a412fc9e874e3acfc1c2065
Alessandro Pecchini <48804063+AlessandroPecchini@users.noreply.github.com>
2022-02-12 16:56:14 +0100

    done some plots and normalized

--- exploratory.R
+++ exploratory.R
@@ -1,4 +1,4 @@
-b2 = read.csv('data/cell_signaling/b2camp.csv')
+b2 = read.csv('data/cell_signaling/b2camp.csv', colClasses = 'num.with.commas')
 names(b2)
 praf = as.numeric(b2$praf)
 pmek = as.numeric(b2$pmek)
@@ -15,7 +15,18 @@ for (n in names(b2)){
 df = read.csv('data/cell_signaling/cd3cd28+aktinhib.csv', colClasses = 'num.with.commas')
 par(mfrow=c(4,3))
 for (n in names(df)){
-  hist(as.numeric(df[[n]]), main=n, breaks = 30)
+  data = df[[n]]
+  scaled = scale(data)
+  normalized = normalize(data)
+  
+  data_hist = hist(scaled, breaks=30,plot=F)
+  norm_data_hist = hist(normalized, breaks=30,plot=F)
+  
+  hist(scaled, main=n, breaks = 30, col = rgb(0,1,0, alpha = 0.6),
+       xlim=c(min(min(data_hist$breaks),min(norm_data_hist$breaks)), max(max(data_hist$breaks),max(norm_data_hist$breaks))),
+       ylim=c(0, max(max(data_hist$counts),max(norm_data_hist$counts)))
+  )
+  hist(normalized, add=T, col = rgb(0,0,1,alpha=0.3))
 }
 plot(1:5, 5:9)
 
@@ -38,3 +49,13 @@ for(fname in list.files(base_dir, recursive = T, full.names = F)){
   dev.off()
 }
 
+boxcox.transform= function(data){
+  bx = boxcox(data~1, plotit = F)
+  lambda = bx$x[which(bx$y==max(bx$y))[1]]
+  if(lambda != 0)
+    return((data^lambda-1)/lambda)
+  else
+    return(log(data))
+}
+normalize = function(data)
+  return(scale(boxcox.transform(data)))
