e8563f2f4795603b9e024ac5a82d5187950ffbcf
PliniLeonardo <plinileonardo@gmail.com>
2022-02-19 15:14:02 +0100

    rifatto tabelle e inserito grafici

--- Leonardo.R
+++ Leonardo.R
@@ -65,10 +65,10 @@ log_likelihood = function(X, A, sigma.square){
 }
 
 
-inner_LRT_function.paths = function(X, X.tr, X.te, D, mu ){
+inner_LRT_function.paths = function(X, X.tr, X.te, D, mu, alpha ){
   
   # Computing all the A.h0 over the sparsity parameters 'k'
-  A.mles = lapply(1:sum(D), function(mu)MLEdag(X, D=D, tau=0.35, mu=mu, rho=1.2, trace_obj = F))
+  A.mles = lapply(1:sum(D), function(mu)MLEdag(X, D=D, tau=alpha, mu=mu, rho=1.2, trace_obj = F))
   A.h0s  = lapply(A.mles, function(a)a$A.H0)
   A.h1s  = lapply(A.mles, function(a)a$A.H1)
   
@@ -89,8 +89,8 @@ inner_LRT_function.paths = function(X, X.tr, X.te, D, mu ){
   
 }
 
-inner_LRT_function.links = function(X, X.tr, X.te, D, mu){
-  tmp = MLEdag(X = X, D = D, tau = 0.35, mu = mu, 
+inner_LRT_function.links = function(X, X.tr, X.te, D, mu, alpha){
+  tmp = MLEdag(X = X, D = D, tau = alpha, mu = mu, 
                rho = 1.2, trace_obj = FALSE)
   A.h0 = tmp$A.H0
   A = tmp$A.H1
@@ -101,18 +101,18 @@ inner_LRT_function.links = function(X, X.tr, X.te, D, mu){
   return(log_likelihood(X.tr, A, sigma.unconstrained)-log_likelihood(X.tr, A.h0, sigma.h0))
 }
 
-log.LRT = function(X,D, links=T, mu){
+log.LRT = function(X,D, links=T, mu,alpha){
   n=dim(X)[1]
   X.tr = X[1:(n%/%2),]
   X.te = X[(n%/%2+1):n,]
   
   if(links){
-    U_n = inner_LRT_function.links(X, X.tr, X.te, D, mu)
-    U_n.swap = inner_LRT_function.links(X, X.te, X.tr, D, mu)
+    U_n = inner_LRT_function.links(X, X.tr, X.te, D, mu, alpha)
+    U_n.swap = inner_LRT_function.links(X, X.te, X.tr, D, mu, alpha)
   }
   else{
-    U_n = inner_LRT_function.paths(X, X.tr, X.te, D, mu)
-    U_n.swap = inner_LRT_function.paths(X, X.te, X.tr, D,mu)
+    U_n = inner_LRT_function.paths(X, X.tr, X.te, D, mu, alpha)
+    U_n.swap = inner_LRT_function.paths(X, X.te, X.tr, D,mu, alpha)
   }
   return(list(links = links, U_n = U_n, W_n = (log((exp(U_n)+exp(U_n.swap))/2))))
 }
@@ -150,7 +150,7 @@ U_n <- rep(NA, M)
 W_n <- rep(NA, M)
 for(i in 1:M) {
   X   <- matrix( rnorm(n*p), n, p) %*% t(solve(diag(p) - A) )
-  temp=log.LRT(X,D, links=T,mu=1.2)
+  temp=log.LRT(X,D, links=T,mu=1.2,alpha=alpha)
   U_n[i]=temp$U_n
   W_n[i]=temp$W_n
   
@@ -160,7 +160,7 @@ for(i in 1:M) {
 
 cat('alpha is equal to for U ', contU/M,'alpha is equal to for W',contW/M)
 
-# 0.092 and 0.103
+# 0.049 and 0.053
 
 # POWER -------------------------------------------------------------------
 
@@ -173,6 +173,9 @@ sparsity=2/p
 D <- matrix(0, p, p)
 D[2:p,1]=1
 D[3:p,2]=1
+D[2,8=1]
+D[5,6]=1
+D[8,9]=1
 
 ### Adjacency Matrix >> Hub
 # All connected to 1, NO EDGE between p-2 >> **NOT COMPATIBLE** with H0
@@ -186,6 +189,7 @@ A[8,9]=1
 A[2:p,1]=1
 A[3:p,2]=1
 
+
 # SIMULATION
 contU=0
 contW=0
@@ -195,7 +199,7 @@ U_n <- rep(NA, M)
 W_n <- rep(NA, M)
 for(i in 1:M) {
   X   <- matrix( rnorm(n*p), n, p) %*% t(solve(diag(p) - A) )
-  temp=log.LRT(X,D, links=T,mu=1.2)
+  temp=log.LRT(X,D, links=T,mu=1.2, alpha=alpha)
   U_n[i]=temp$U_n
   W_n[i]=temp$W_n
   
@@ -204,7 +208,7 @@ for(i in 1:M) {
 }
 
 cat('1-beta is equal to for U: ', contU/M,'   ,1-beta is equal to for W:',contW/M)
-
+#0.741 and 0.859
 
 # PART 4 ------------------------------------------------------------------
 
@@ -286,6 +290,7 @@ cat("U_n:", U_n, "   W_n:", W_n)
 # PART 5 FINAL ------------------------------------------------------------
 ##
 #PROVE BOXCOX
+library(MASS)
 data=read.csv("data/cell_signaling/normalized_and_scaled/pma_normalized.csv",header=T,sep=",")
 X=data.matrix(data, rownames.force = NA)
 #data will be our X matrix
@@ -324,22 +329,22 @@ D[4,9] = 1
 alpha=0.05
 MU=100
 
-temp=log.LRT(X,D, links=T,mu=MU)
+temp=log.LRT(X,D, links=T,mu=MU,alpha=alpha)
 U_n=temp$U_n
 W_n=temp$W_n
 
 if(U_n>log(1/alpha)) cat(" We can reject the null hypothesis") else cat(" We can not reject the null hypothesis")
 ## OKKKK, we accept the null hypothesis
 
-out=MLEdag(X=X,D=D,tau=0.35, mu=MU, rho=1.2, trace_obj = F)
+out=MLEdag(X=X,D=D,tau=alpha, mu=MU, rho=1.2, trace_obj = F)
 out$pval
 # they reject the null hypothesis
 ## DIFFERENCE
 #first connection
-tab <- matrix(c(0.0349,-11.13,-10.02,
-                0.0349,-11.13,-10.02,
-                0.0349,-11.13,-10.02,
-                0.0349,-11.13,-10.02,), ncol=3, byrow=TRUE)
+tab <- matrix(c(1,-16.19,-15.48,
+                1,-16.19,-15.48,
+                0.21,-18.48,-14.64,
+                0.21,-18.48,-14.64), ncol=3, byrow=TRUE)
 colnames(tab) <- c('pvalue_MLE_dag','Un','Wn')
 rownames(tab) <- c('k=0.01','k=0.1','k=1','k=10')
 tab <- as.table(tab)
@@ -351,22 +356,56 @@ tab <- as.table(tab)
 D <- matrix(0, p, p)
 D[9,11] = 1
 alpha=0.05
-MU=100
-temp=log.LRT(X,D, links=T,mu=MU)
+MU=0.01
+
+U_n=rep(NA,41)
+W_n=rep(NA,41)
+pvalues=rep(NA,41)
+a=seq(0,2,0.05)
+j=1
+for (i in a){
+  temp=log.LRT(X,D, links=T,mu=i, alpha=alpha)
+  U_n[j]=temp$U_n
+  W_n[j]=temp$W_n
+  out=MLEdag(X,D=D,tau=alpha, mu=i, rho=1.2, trace_obj = F)
+  pvalues[j]=out$pval
+  j=j+1
+}
+
+# plot the first curve by calling plot() function
+# First curve is plotted
+plot(a, pvalues, type="o", col="blue", pch="o", ylim=c(-20,20),lty=1 )
+
+# Add second curve to the same plot by calling points() and lines()
+# Use symbol '*' for points.
+points(a, U_n, col="red", pch="*",ylim=c(-20,20))
+lines(a, U_n, col="red",lty=2)
+
+# Add Third curve to the same plot by calling points() and lines()
+# Use symbol '+' for points.
+points(a, W_n, col="green",ylim=c(-20,20),pch="+")
+lines(a, W_n, col="green", lty=3)
+
+# Adding a legend inside box at the location (2,40) in graph coordinates.
+# Note that the order of plots are maintained in the vectors of attributes.
+legend("bottomright",legend=c("pvalues","U_n","W_n"), col=c("blue","red","green"),
+       pch=c("o","*","+"),lty=c(1,2,3),cex=0.5, ncol=1)
+
+'''
+temp=log.LRT(X,D, links=T,mu=MU, alpha=alpha)
 U_n=temp$U_n
 W_n=temp$W_n
 
 if(U_n>log(1/alpha)) cat(" We can reject the null hypothesis") else cat(" We can not reject the null hypothesis")
 #dovremmo rigettare
 #male
-out=MLEdag(X,D=D,tau=0.35, mu=MU, rho=1.2, trace_obj = F)
+out=MLEdag(X,D=D,tau=alpha, mu=MU, rho=1.2, trace_obj = F)
 out$pval
-
-
-tab <- matrix(c(5.21e-10,20.38,19.69,
-                ,5.21e-10,22.7,22.01
-                ,1.72e-06,22.7,22.01
-                ,1.72e-06,22.7,22.01,exp(-3.189)), ncol=3, byrow=TRUE)
+'''
+tab <- matrix(c(1,-16.19,-15.48,
+                ,1,-16.19,-15.48
+                ,6.0e-11,17.05,16.36
+                ,6.0e-11,17.05,16.36,exp(-3.189)), ncol=3, byrow=TRUE)
 colnames(tab) <- c('pvalue_MLE_dag','Un','Wn')
 rownames(tab) <- c('k=0.01','k=0.1','k=1','k=10')
 tab <- as.table(tab)
@@ -377,32 +416,30 @@ tab <- as.table(tab)
 D <- matrix(0, p, p)
 D[9,5] = 1
 alpha=0.05
-U_n=rep(NA,100)
-W_n=rep(NA,100)
-pvalues=rep(NA,100)
+#U_n=rep(NA,100)
+#W_n=rep(NA,100)
+#pvalues=rep(NA,100)
 MU=100
 
-for ( i in 1:100){
-  MU=i
-  temp=log.LRT(X,D, links=T,mu=MU)
-  U_n[i]=temp$U_n
-  W_n[i]=temp$W_n
-  
-  #if(U_n>log(1/alpha)) cat(" We can reject the null hypothesis") else cat(" We can not reject the null hypothesis")
-  #dovremmo accettare H0
-  #ok
-  out=MLEdag(X,D=D,tau=0.35, mu=MU, rho=1.2, trace_obj = F)
-  pvalues[i]=out$pval
-  
-}
+#for ( i in 1:100){
+#MU=i
+temp=log.LRT(X,D, links=T,mu=MU,alpha=alpha)
+U_n=temp$U_n
+W_n=temp$W_n
 
-plot(pvalues,col="red")
-plot(U_n,add=TRUE,col="blue")
+#if(U_n>log(1/alpha)) cat(" We can reject the null hypothesis") else cat(" We can not reject the null hypothesis")
+#dovremmo accettare H0
+#ok
+out=MLEdag(X,D=D,tau=alpha, mu=MU, rho=1.2, trace_obj = F)
+out$pval
+  
+#}
 
-tab <- matrix(c(0.65,-12.61,-11.80
-                ,0.65,-15.18,-14.18
-                ,0.65,-12.61,-11.8
-                ,0.65,-12.61,-11.8), ncol=3, byrow=TRUE)
+
+tab <- matrix(c(1,-16.19,-15.48
+                ,0.60,-15.80,-14.80
+                ,0.60,-15.80,-14.80
+                ,0.60,-15.80,-14.80), ncol=3, byrow=TRUE)
 colnames(tab) <- c('pvalue_MLE_dag','Un','Wn')
 rownames(tab) <- c('k=0.01','k=0.1','k=1','k=10')
 tab <- as.table(tab)
@@ -416,21 +453,55 @@ D[9,1] = 1
 D[1,2] = 1
 D[2,6] = 1
 alpha=0.05
-MU=100
+MU=0.1
 #first connection
-temp=log.LRT(X,D, links=F,mu=MU)
+'''
+temp=log.LRT(X,D, links=F,mu=MU,alpha=alpha)
 U_n=temp$U_n
 W_n=temp$W_n
 cat("U_n:", U_n, "   W_n:", W_n)
-# non rigettiamo l'hp nulla quindi ok
-out=MLEdag(X,D=D,tau=0.35, mu=MU, rho=1.2, trace_obj = F)
+
+out=MLEdag(X,D=D,tau=alpha, mu=MU, rho=1.2, trace_obj = F)
 out$pval
+'''
+U_n=rep(NA,41)
+W_n=rep(NA,41)
+pvalues=rep(NA,41)
+a=seq(0,2,0.05)
+j=1
+for (i in a){
+  temp=log.LRT(X,D, links=F,mu=i, alpha=alpha)
+  U_n[j]=temp$U_n
+  W_n[j]=temp$W_n
+  out=MLEdag(X,D=D,tau=alpha, mu=i, rho=1.2, trace_obj = F)
+  pvalues[j]=out$pval
+  j=j+1
+}
+
+# plot the first curve by calling plot() function
+# First curve is plotted
+plot(a, pvalues, type="o", col="blue", pch="o", ylim=c(0,1),lty=1 )
+'''
+# Add second curve to the same plot by calling points() and lines()
+# Use symbol '*' for points.
+points(a, U_n, col="red", pch="*",ylim=c(-100,20))
+lines(a, U_n, col="red",lty=2)
+
+# Add Third curve to the same plot by calling points() and lines()
+# Use symbol '+' for points.
+points(a, W_n, col="green",ylim=c(-100,20),pch="+")
+lines(a, W_n, col="green", lty=3)
+'''
+# Adding a legend inside box at the location (2,40) in graph coordinates.
+# Note that the order of plots are maintained in the vectors of attributes.
+legend("bottomleft",legend=c("pvalues"), col=c("blue"),
+       pch=c("o"),lty=c(1),cex=0.4, ncol=1)
 
 
-tab <- matrix(c(2.9e-96,-71.65,-55.68
-                ,2.9e-96,-71.65,-55.68
-                ,2.9e-96,-71.65,-55.68
-                ,2.9e-96,-71.65,-55.68), ncol=3, byrow=TRUE)
+tab <- matrix(c(0.82,-71.56,-56.09
+                ,1,-71.56,-56.09
+                ,1.62e-106,-71.56,-56.09
+                ,1.62e-106,-71.56,-56.09), ncol=3, byrow=TRUE)
 colnames(tab) <- c('pvalue_MLE_dag','Un','Wn')
 rownames(tab) <- c('k=0.01','k=0.1','k=1','k=10')
 tab <- as.table(tab)
@@ -440,7 +511,7 @@ tab <- as.table(tab)
 #import concatenated
 data=read.csv("data/cell_signaling/normalized_and_scaled/concatenated_normalized.csv",header=T,sep=",")
 X=data.matrix(data, rownames.force = NA)
-#data will be our X matrix
+#data will be our X matrixSu
 p=11
 #transformation
 
@@ -456,21 +527,20 @@ D[4,9] = 1
 alpha=0.05
 MU=100
 
-temp=log.LRT(X,D, links=T,mu=MU)
+temp=log.LRT(X,D, links=T,mu=MU,alpha=alpha)
 U_n=temp$U_n
 W_n=temp$W_n
 
 if(U_n>log(1/alpha)) cat(" We can reject the null hypothesis") else cat(" We can not reject the null hypothesis")
-## OKKKK, we accept the null hypothesis
 
-out=MLEdag(X,D=D,tau=0.35, mu=MU, rho=1.2, trace_obj = F)
+out=MLEdag(X,D=D,tau=alpha, mu=MU, rho=1.2, trace_obj = F)
 out$pval
 
 ## DIFFERENCE
-tab <- matrix(c(1.67e-113,-883,-Inf
-                ,1.67e-113,-883,-Inf
-                ,1.67e-113,-883,-Inf
-                ,1.67e-113,-883,-Inf, ncol=3, byrow=TRUE)
+tab <- matrix(c(1,-625.26,-534.54
+                ,1,-625.26,-534.54
+                ,1,-466.80,-407.60
+                ,1,-466.80,-407.60, ncol=3, byrow=TRUE)
 colnames(tab) <- c('pvalue_MLE_dag','Un','Wn')
 rownames(tab) <- c('k=0.01','k=0.1','k=1','k=10')
 tab <- as.table(tab)
@@ -482,23 +552,57 @@ tab <- as.table(tab)
 D <- matrix(0, p, p)
 D[9,11] = 1
 alpha=0.05
-MU=100
-
-temp=log.LRT(X,D, links=T,mu=MU)
+MU=0.2
+temp=log.LRT(X,D, links=T,mu=MU,alpha=alpha)
 U_n=temp$U_n
 W_n=temp$W_n
 
 if(U_n>log(1/alpha)) cat(" We can reject the null hypothesis") else cat(" We can not reject the null hypothesis")
 #dovremmo rigettare
 #male
-out=MLEdag(X,D=D,tau=0.35, mu=MU, rho=1.2, trace_obj = F)
+out=MLEdag(X,D=D,tau=alpha, mu=MU, rho=1.2, trace_obj = F)
 out$pval
 
 
-tab <- matrix(c(0,-466.16,-466.86
-                ,0,-466.16,-466.86
-                ,0,-466.16,-466.86
-                ,0,-466.16,-466.86, ncol=3, byrow=TRUE)
+U_n=rep(NA,51)
+W_n=rep(NA,51)
+pvalues=rep(NA,51)
+a=seq(0,10,0.2)
+j=1
+for (i in a){
+  temp=log.LRT(X,D, links=T,mu=i, alpha=alpha)
+  U_n[j]=temp$U_n
+  W_n[j]=temp$W_n
+  out=MLEdag(X,D=D,tau=alpha, mu=i, rho=1.2, trace_obj = F)
+  pvalues[j]=out$pval
+  j=j+1
+}
+
+par(mfrow = c(3,1),mar=c(2,2,1,1))
+# plot the first curve by calling plot() function
+# First curve is plotted
+plot(a, pvalues, type="o", col="blue", pch="o", ylim=c(-0.5,1.5),main="pvalues" )
+
+# Add second curve to the same plot by calling points() and lines()
+# Use symbol '*' for points.
+plot(a, U_n, col="red",main="U_n",ylim=c(-500,500))
+#points(a, U_n, col="red", pch="*",ylim=c(-300,100),main="U_n")
+lines(a, U_n, col="red")
+
+# Add Third curve to the same plot by calling points() and lines()
+# Use symbol '+' for points.
+plot(a, W_n, col="green",main="W_n",ylim=c(-500,500))
+#points(a, W_n, col="green",ylim=c(-300,100),pch="+")
+lines(a, W_n, col="green")
+
+
+
+
+
+tab <- matrix(c(0,-297.12,-297.81
+                ,0,-297.12,-297.81
+                ,0,36.50,35.81
+                ,0,-297.12,-297.81, ncol=3, byrow=TRUE)
 colnames(tab) <- c('pvalue_MLE_dag','Un','Wn')
 rownames(tab) <- c('k=0.01','k=0.1','k=1','k=10')
 tab <- as.table(tab)
@@ -511,21 +615,21 @@ D[9,5] = 1
 alpha=0.05
 MU=100
 
-temp=log.LRT(X,D, links=T,mu=MU)
+temp=log.LRT(X,D, links=T,mu=MU,alpha=alpha)
 U_n=temp$U_n
 W_n=temp$W_n
 
 if(U_n>log(1/alpha)) cat(" We can reject the null hypothesis") else cat(" We can not reject the null hypothesis")
 #dovremmo accettare H0
 #ok
-out=MLEdag(X,D=D,tau=0.35, mu=MU, rho=1.2, trace_obj = F)
+out=MLEdag(X,D=D,tau=alpha, mu=MU, rho=1.2, trace_obj = F)
 out$pval
 
 
-tab <- matrix(c(3.37e-30,-1342.46,-Inf
-                ,3.37e-30,-1342.46,-Inf
-                ,3.37e-30,-1342.46,-Inf
-                ,3.37e-30,-1342.46,-Inf), ncol=3, byrow=TRUE)
+tab <- matrix(c(1,-625.26,-534.54
+                ,1,-625.26,-534.54
+                ,1,-466.80,-407.60
+                ,0.84,-585.54,-504.55), ncol=3, byrow=TRUE)
 colnames(tab) <- c('pvalue_MLE_dag','Un','Wn')
 rownames(tab) <- c('k=0.01','k=0.1','k=1','k=10')
 tab <- as.table(tab)
@@ -541,19 +645,56 @@ D[2,6] = 1
 alpha=0.05
 MU=100
 
-temp=log.LRT(X,D, links=F,mu=MU)
+
+temp=log.LRT(X,D, links=F,mu=MU,alpha=alpha)
 U_n=temp$U_n
 W_n=temp$W_n
 cat("U_n:", U_n, "   W_n:", W_n)
 # non rigettiamo l'hp nulla quindi ok
-out=MLEdag(X,D=D,tau=0.35, mu=MU, rho=1.2, trace_obj = F)
-out$pval
+#out=MLEdag(X,D=D,tau=alpha, mu=MU, rho=1.2, trace_obj = F)
+#out$pval
 
 
-tab <- matrix(c(0,-1115.38,-Inf
-                ,0,-1115.38,-Inf
-                ,0,-1115.38,-Inf
-                ,0,-1115.38,-Inf), ncol=3, byrow=TRUE)
+
+U_n=rep(NA,41)
+W_n=rep(NA,41)
+pvalues=rep(NA,41)
+a=seq(0,100,1)
+j=1
+for (i in a){
+  #temp=log.LRT(X,D, links=F,mu=i, alpha=alpha)
+  #U_n[j]=temp$U_n
+  #W_n[j]=temp$W_n
+  out=MLEdag(X,D=D,tau=alpha, mu=i, rho=1.2, trace_obj = F)
+  pvalues[j]=out$pval
+  j=j+1
+}
+
+par(mfrow=c(1,1),mar=c(5,2,4.1,2.1))
+# plot the first curve by calling plot() function
+# First curve is plotted
+plot(a, pvalues, type="l", col="blue", pch="o", ylim=c(0,1),lty=1 ,main="pvalues")
+
+# Add second curve to the same plot by calling points() and lines()
+# Use symbol '*' for points.
+points(a, U_n, col="red", pch="*",ylim=c(-100,20))
+lines(a, U_n, col="red",lty=2)
+
+# Add Third curve to the same plot by calling points() and lines()
+# Use symbol '+' for points.
+points(a, W_n, col="green",ylim=c(-100,20),pch="+")
+lines(a, W_n, col="green", lty=3)
+
+# Adding a legend inside box at the location (2,40) in graph coordinates.
+# Note that the order of plots are maintained in the vectors of attributes.
+legend("bottomleft",legend=c("pvalues"), col=c("blue"),
+       pch=c("o"),lty=c(1),cex=0.4, ncol=1)
+
+
+tab <- matrix(c(1,-924.26,-Inf
+                ,1,-924.26,-Inf
+                ,0,-924.26,-Inf
+                ,0,-924.26,-Inf), ncol=3, byrow=TRUE)
 colnames(tab) <- c('pvalue_MLE_dag','Un','Wn')
 rownames(tab) <- c('k=0.01','k=0.1','k=1','k=10')
 tab <- as.table(tab)
